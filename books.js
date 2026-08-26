6c1e3178dc61558c775d8f02a77d0b789abda4a9
2008127 <fli67071@gmail.com>
2026-08-27 06:16:44 +0800

    shelf owner-curated read-only from books.js

---
+++ books.js
@@ -0,0 +1,8 @@
+/* 雨后影视社 · 站长书单
+   站长要加/删书，只需要改这里的数组，页面代码不用动。
+   格式：{name:'片名', author:'作者'} */
+window.STATION_BOOKS = [
+  {name:'千与千寻', author:'宫崎骏'},
+  {name:'星际穿越', author:'诺兰'},
+  {name:'你的名字', author:'新海诚'},
+];
